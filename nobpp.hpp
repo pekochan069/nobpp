6f87485bbac55146e2bf2650fcf267e21dcd01e7
pekochan069 <pekochan069@gmail.com>
2025-01-10 17:55:37 +0900

    Reduce sleep duration to 10ms

--- nobpp.hpp
+++ nobpp.hpp
@@ -992,7 +992,7 @@ public:
                 break;
             }
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
     }
 
